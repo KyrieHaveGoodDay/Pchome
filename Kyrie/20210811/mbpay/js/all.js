6317be5327b8d9f694e4d52cb08b68e0fa9f0024
kyriehavegoodday <healthymbear@gmail.com>
2021-08-11 16:10:49 +0800

    行動支付banner區塊

---
+++ Kyrie/20210811/mbpay/js/all.js
@@ -0,0 +1,61 @@
+$(function () {
+
+  // [側邊選單] 
+  var $sidenav = $('.sidenav');
+  // [側邊選單]--// 收合
+  $sidenav.on('click', '.sidenav__btn a', function (e) {
+    e.preventDefault();
+    $(this).parents('.sidenav').toggleClass('sidenav--hide');
+  })
+
+  // [右邊選單]
+  var $rightNav = $('.sidenav--right');
+  // [右邊選單]--// 側選單是
+  var $sidenavTop = $rightNav.length > 0 ? $rightNav.offset().top : 0;
+  // [右邊選單]--// 手機版置頂
+  function rightnavFixedTop() {
+    var $windowTop = $(window).scrollTop();
+    if ($windowTop > $sidenavTop) {
+      $sidenav.addClass('fixed');
+      $('.main').addClass('addPadding')
+    }
+    else {
+      $sidenav.removeClass('fixed');
+      $('.main').removeClass('addPadding')
+    }
+  }
+
+  // [右邊GoTop]--// 滾動出現
+  function goTopShow() {
+    var $windowTop = $(window).scrollTop();
+    $windowTop >= 100 ? $('.gotop').addClass('show') : $('.gotop').removeClass('show');
+  }
+  // [右邊GoTop]--// gotop
+  $('.gotop').on('click', function () {
+    $('html,body').animate({ scrollTop: '0px' }, 300);
+  });
+
+  // [錨點]--// 判斷滑動位置
+  $('a[href^="#"]').on('click', function (e) {
+    e.preventDefault();
+    var headerH = $('.header').height();
+    var sidenavH = $('.sidenav').height();
+    var targetTop = $($(this).attr('href')).offset().top;
+    var scrollPos = $(window).width() >= 768 ? targetTop : targetTop - headerH - sidenavH;
+    $('html, body').stop().animate({
+      scrollTop: scrollPos
+    }, 300);
+  });
+
+
+  $(window).on('scroll', function () {
+    goTopShow();
+    $(window).width() < 768 && rightnavFixedTop();
+  }).scroll();
+
+
+
+});
+
+// banner slider
+var swiper = new Swiper(".mySwiper", {});
